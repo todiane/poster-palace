81e8af42a4f3bea1701d148234464a46d34548c6
todiane Corri <dcorriette@gmail.com>
2023-12-22 14:55:27 +0000

    Add diverse design events page plus terms and conditions. Adjust other pages as needed with link to new pages

--- marketing/urls.py
+++ marketing/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.subscription, name="subscription"),
+    path("poster_event/", views.poster_event, name="poster_event"),
+    path("event_terms/", views.event_terms, name="event_terms"),
 ]
--- marketing/views.py
+++ marketing/views.py
@@ -6,3 +6,15 @@ def subscription(request):
  
     return render(request, "marketing/subscriptions.html")
 
+
+def poster_event(request):
+    """ View to return the Diverse Design events page """
+ 
+    return render(request, "marketing/poster_event.html")
+
+
+def event_terms(request):
+    """ View to return the Diverse Design events page """
+ 
+    return render(request, "marketing/event_terms.html")
+
