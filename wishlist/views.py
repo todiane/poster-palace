ecfdbeaaac2b97c3101f91a15701b10c56c80718
todiane Corri <dcorriette@gmail.com>
2023-12-26 15:26:34 +0000

    Add products/image/description to wishlist.html page. include delete button (not working yet, needs amending)

--- wishlist/views.py
+++ wishlist/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseServerError
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from django.http import JsonResponse
 
 
 from .models import WishList
@@ -50,7 +51,8 @@ def delete_wish(request):
     """remove poster from wishlist"""
     if request.method == 'POST':
         item_id = request.POST.get('item-id')
-        wish_item = get_object_or_404(WishList, pk=item_id)
+        wish_item = get_object_or_404(WishList, pk=item_id, user_id=request.user)
         wish_item.delete()
-        messages.success(request, f'{wish_item.product.name} has been deleted from your wishlist')
-    return HttpResponseRedirect(reverse('wishlist'))
+        messages.success(request, f'{item.product_id.name} has been deleted from your wishlist')
+        return JsonResponse({'message': 'Item deleted successfully.'})
+    return JsonResponse({'message': 'Invalid request.'}, status=400)
